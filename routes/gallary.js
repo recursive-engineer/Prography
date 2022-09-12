a47e2536ce6eea8fabe937db78e401c4e68c788c
murata0531 <coless115@gmail.com>
2022-09-12 11:55:12 +0900

    homeをgallaryへ変更

--- routes/gallary.js
+++ routes/gallary.js
@@ -1,6 +1,6 @@
-var express = require("express");
+const express = require("express");
 
-var router = express.Router();
+let router = express.Router();
 
 router.get("/", async function (req, res, next) {
   res.render("gallery",{
